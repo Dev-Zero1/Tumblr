42f53091129c66889cbcfbbffa6ad09d97b4bff4
Dev-Zero1 <DevMail.exe@gmail.com>
2018-11-09 22:40:05 -0800

    Debugging Stage

---
+++ Tumblr/photoCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  photoCellTableViewCell.swift
+//  Tumblr
+//
+//  Created by administrator on 11/9/18.
+//  Copyright © 2018 administrator. All rights reserved.
+//
+
+import UIKit
+
+class photoCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
